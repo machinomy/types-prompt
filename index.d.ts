9bd4ea43baf223772e8c68b962735f49c0de8496
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-26 19:28:01 +0300

    Improve typings for IDE syntax highlighting.

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,16 @@
 declare module 'prompt' {
-  namespace PromptNS {
-    let message: string = 'prompt'
-    let colors: boolean = true
+  class Prompt {
+    message: string = 'prompt'
+    colors: boolean = true
 
-    function start (): void
-    function pause (): void
-    function stop (): void
-    function resume (): void
-    function history (search: Number | string): Object
-    function get<T> (schema: Array | Object | string, callback: (err: Error, result: T) => void): Object
+    start (): void
+    pause (): void
+    stop (): void
+    resume (): void
+    history (search: Number | string): Object
+    get<T> (schema: Array | Object | string, callback: (err: Error, result: T) => void): Object
   }
 
-  export = PromptNS
+  const prompt: Prompt
+  export default prompt
 }
